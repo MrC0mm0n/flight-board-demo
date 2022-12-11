22e69658d002de4cd0b8a4eeaf7499b60d8b8faf
Shaik Mohammed Rawoof <inform.smohdr88@gmail.com>
2022-12-11 18:08:10 -0500

    added image

--- backend/src/main/java/ca/yyz/repository/DepartureRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.yyz.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ca.yyz.model.Departure;
-
-public interface DepartureRepository extends JpaRepository<Departure, String> {
-
-}
